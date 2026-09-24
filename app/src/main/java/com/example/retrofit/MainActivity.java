c0128f0be1308f99fac72cc5dd9c8b11e18f0b9d
anastv4 <anastazjafil.163@gmail.com>
2026-09-24 08:55:43 +0200

    widok ale dokonczyc

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.retrofit;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RadioGroup radioGroupPytania;
+    RadioButton radioButtonA, radioButtonB, radioButtonC;
+    Button buttonNastepne;
+    TextView textViewTrescPytania;
+    ArrayList<Pytanie> listaPytan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
